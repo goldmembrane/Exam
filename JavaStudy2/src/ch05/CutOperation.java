7781899b6867a45c542afa5b5f3d15aea5b58c42
soundmusic <wisu69@naver.com>
2019-04-10 09:35:59 +0900

    막대기 문제 해결

--- JavaStudy2/src/ch05/CutOperation.java
+++ JavaStudy2/src/ch05/CutOperation.java
@@ -1,24 +1,36 @@
 package ch05;
 
-import java.util.Scanner;
-
 public class CutOperation {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int stick = s.nextInt();
-		int min = Integer.MAX_VALUE;
-		int count = 0;
-		
-		int[] cutStick = new int[stick];
-		for (int i = 0; i < cutStick.length; i++) {
-			if(cutStick[i]<min) {
-				min = cutStick[i];
-				
+		// 막대기 배열의 요소들을 설정한다.
+		int[] stick = { 2, 4, 4, 5, 6, 8 };
+		while (true) {
+			// 막대기 배열 중 0보다 큰 요소들의 개수를 구한다.
+			int count = 0;
+			for (int i = 0; i < stick.length; i++) {
+				if (stick[i] > 0) {
+					count++;
+				}
 			}
-		}
-		System.out.println(min);
-		s.close();
-	}
+			System.out.println(count);
+			if(count <= 1) {
+				break;
+			}
+			// 막대기 배열 속 최소값을 구한다.
+			int min = Integer.MAX_VALUE;
+			for (int i = 0; i < stick.length; i++) {
+				if (stick[i] < min && stick[i] > 0) {
+					min = stick[i];
+				}
+			}
 
+			// 막대기 배열의 요소들을 최소값만큼 감소시킨다.
+			for (int i = 0; i < stick.length; i++) {
+				stick[i] = stick[i] - min;
+			}
+			
+		}
+		
+	}
 }
